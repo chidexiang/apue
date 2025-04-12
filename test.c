50cbc5d2c5b38c00d774b6cbe9562ea2ed1d9224
guanjiaxu <m15540097929@163.com>
2025-04-12 17:47:19 +0800

    is a test file

---
+++ test.c
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2025 LingYun<iot25@lingyun>
+ *                  All rights reserved.
+ *
+ *       Filename:  test.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(12/04/25)
+ *         Author:  LingYun <iot25@lingyun>
+ *      ChangeLog:  1, Release initial version on "12/04/25 17:45:51"
+ *                 
+ ********************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("change branch success\n");
+	return 0;
+}
